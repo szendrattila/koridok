90df3f48775b24754237da000d879b9131029318
szendrattila <71691690+szendrattila@users.noreply.github.com>
2022-03-03 17:21:29 +0000

    gyakorlas adammal

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ csapat;versenyzo;eletkor;palya;korido;kor
 Versenylovak;Fürge Ferenc;29;Gran Prix Circuit;00:01:11;1
 Versenylovak;Fürge Ferenc;29;Gran Prix Circuit;00:01:05;2
 """
-
+"""
 class Korido:
     def __init__(self, sor):
         csapat, versenyzo, eletkor, palya, korido, kor = sor.strip().split(";")
@@ -27,9 +27,12 @@ with open("autoverseny.txt", encoding="utf-8") as f:
 print(f"3. feladat: {len(lista)}")
 #4.feladat
 """
+"""
+
 for sor in lista:
     if sor.versenyzo == "Fürge Ferenc" and sor.palya == "Gran Prix Circuit" and sor.kor == 3:
         print(f"4. feladat: {sor.ora * 3600 + sor.perc * 60 + sor.masodperc } másodperc")
+"""
 """
 masodperc = [sor.ora * 3600 + sor.perc * 60 + sor.masodperc for sor in lista if sor.versenyzo == "Fürge Ferenc" and sor.palya == "Gran Prix Circuit" and sor.kor == 3]
 print(f"4. feladat: {masodperc[0]} másodperc")
@@ -45,4 +48,49 @@ for sor in lista:
     else:
         print("6. feladat Nincs ilyen versenyző az állományban")
     break
+"""
+#########################################################################
+"""
+
+csapat;versenyzo;eletkor;palya;korido;kor
+Versenylovak;Fürge Ferenc;29;Gran Prix Circuit;00:01:11;1
+"""
+class Korido:
+    def __init__(self, sor):
+        csapat, versenyzo, eletkor, palya, korido, kor = sor.strip().split(";")
+        self.csapat = csapat
+        self.versenyzo = versenyzo
+        self.eletkor = eletkor
+        self.palya = palya
+        self.korido = korido
+        self.kor = int(kor)
+        self.ora = int(korido[:2])
+        self.perc = int(korido[3:5])
+        self.masodperc = int(korido[6:8])
+        
+        
+
+lista = []
+with open("autoverseny.txt", encoding="utf-8") as f:
+    f.readline()
+    for sor in f:
+        lista.append(Korido(sor))
+
+#3.fealdat
+print(f"3.feladat: {len(lista)} db ")
+#4.feladat
+hatarozd = [sor.ora * 3600 + sor.perc * 60 + sor.masodperc for sor in lista if sor.versenyzo == "Fürge Ferenc" and sor.palya == "Gran Prix Circuit" and sor.kor == 3]
+print(f"4.feladat: {hatarozd[0]} másodperc")
+#5.feladat
+bekernev = input("Kérek egy nevet: \n")
+#6.feladat
+
+for sor in lista:
+    masodperc = int(sor.ora * 3600 + sor.perc * 60 + sor.masodperc)
+
+    if sor.versenyzo == bekernev:
+        if masodperc > sor:
+            masodperc = sor
+        print(masodperc)
+        
         
